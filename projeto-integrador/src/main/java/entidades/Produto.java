f6b0535e717aafd8765be101f5dfd7ca90813880
sergiodelfino <88910873+sergiodelfino@users.noreply.github.com>
2025-08-27 22:59:35 +0000

    Aula do dia 27/08/2025

---
+++ projeto-integrador/src/main/java/entidades/Produto.java
@@ -0,0 +1,27 @@
+package entidades;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer codigo;
+
+    public String nome;
+
+    public Integer quantidade;
+
+    public Double preco;
+
+    public LocalDate validade;
+
+    @ManyToOne
+    public Categoria categoria;
+}
