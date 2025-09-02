2e6c7852a5384dc9a04fd2e13ed77ae5e7fd67a9
sergiodelfino <88910873+sergiodelfino@users.noreply.github.com>
2025-09-02 01:12:54 +0000

    Aula do Dia 01/09/2025

--- projeto-integrador/src/main/java/entidades/Fabricante.java
+++ projeto-integrador/src/main/java/entidades/Fabricante.java
@@ -1,30 +1,15 @@
 package entidades;
 
-import java.time.LocalDate;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Produto {
+public class Fabricante {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer codigo;
 
     public String nome;
-
-    public Integer quantidade;
-
-    public Double preco;
-
-    public LocalDate validade;
-
-    @ManyToOne
-    public Categoria categoria;
-
-    @ManyToOne
-    public Fabricante fabricante;
 }
---
+++ projeto-integrador/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into Categoria (nome) values ('Bebidas');
+insert into Categoria (nome) values ('Carnes');
+
+insert into Fabricante (nome) values ('JBS');
+insert into Fabricante (nome) values ('The Coca Cola Company');
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Coca Coca 2 Litros', 20, 12.50, '2025-12-20', 1, 2);
