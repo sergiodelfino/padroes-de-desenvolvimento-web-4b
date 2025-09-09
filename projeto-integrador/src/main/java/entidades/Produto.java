2597e87168897ba9c5be47f6f5c954023f9f88dd
sergiodelfino <88910873+sergiodelfino@users.noreply.github.com>
2025-09-09 01:27:30 +0000

    Aula do Dia 08/09/2025

--- projeto-integrador/src/main/java/entidades/Produto.java
+++ projeto-integrador/src/main/java/entidades/Produto.java
@@ -2,6 +2,7 @@ package entidades;
 
 import java.time.LocalDate;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Produto {
+public class Produto extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer codigo;
