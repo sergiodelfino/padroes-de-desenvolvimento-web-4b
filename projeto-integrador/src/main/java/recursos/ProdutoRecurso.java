3d50a2a8431394c8d12c94415a6d380068e67e3f
sergiodelfino <88910873+sergiodelfino@users.noreply.github.com>
2025-11-11 00:58:30 +0000

    Aula do Dia 10/11/2025

--- projeto-integrador/src/main/java/recursos/ProdutoRecurso.java
+++ projeto-integrador/src/main/java/recursos/ProdutoRecurso.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import entidades.Produto;
 import io.quarkus.panache.common.Sort;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 @Path("produtos")
@@ -12,5 +14,11 @@ public class ProdutoRecurso {
     @GET
     public List<Produto> listar () {
         return Produto.listAll(Sort.ascending("nome"));
+    }
+
+    @POST
+    @Transactional
+    public void salvar (Produto produto) {
+        produto.persist();
     }
 }
