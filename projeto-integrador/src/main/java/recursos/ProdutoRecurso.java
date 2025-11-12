0c71b49ec736d4703d0fde399b464f3c235bd8cb
sergiodelfino <88910873+sergiodelfino@users.noreply.github.com>
2025-11-12 22:50:56 +0000

    Aula do Dia 12/11/2026

--- projeto-integrador/src/main/java/recursos/ProdutoRecurso.java
+++ projeto-integrador/src/main/java/recursos/ProdutoRecurso.java
@@ -5,9 +5,11 @@ import java.util.List;
 import entidades.Produto;
 import io.quarkus.panache.common.Sort;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
 @Path("produtos")
 public class ProdutoRecurso {
@@ -20,5 +22,16 @@ public class ProdutoRecurso {
     @Transactional
     public void salvar (Produto produto) {
         produto.persist();
+    }
+
+    @DELETE
+    @Path("{codigo}")
+    @Transactional
+    public void excluir (@PathParam("codigo") Integer codigo) {
+        Produto produto = Produto.findById(codigo);
+
+        if (produto != null) {
+            produto.delete();
+        }
     }
 }
